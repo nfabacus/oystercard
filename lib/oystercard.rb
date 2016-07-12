29b84c5d98cc230112451e37760dc77265c0eae8
Sara <sarah.alalaya@gmail.com>
2016-07-12 16:15:30 +0300

    Challenge4 (default balance = 0)

---
+++ lib/oystercard.rb
@@ -0,0 +1,10 @@
+class Oystercard
+
+attr_accessor :balance
+
+def initialize
+  @balance = 0
+end
+
+
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,11 @@
+require "oystercard"
+
 describe Oystercard do
-  
+
+  subject(:card) { Oystercard.new }
+
+  it 'has a default balance of 0' do
+    expect(card.balance).to eq 0
+  end
+
 end
