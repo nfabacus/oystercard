d95a7cbd2f9bb09b004cda8c66532c991fd26ad0
Sara <sarah.alalaya@gmail.com>
2016-07-12 16:33:36 +0300

    Challenge5: add top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,4 +8,9 @@ describe Oystercard do
     expect(card.balance).to eq 0
   end
 
+  it "can be topped up" do
+    random_number = rand(1000)
+    expect{card.top_up(random_number)}.to change{card.balance}.by random_number
+  end
+
 end
