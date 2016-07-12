713233c6e5f07736a9be14b64f54658b6ec59b01
noby <n.fujioka@abacusmaths.info>
2016-07-12 11:57:23 +0100

    top_up method added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,4 +4,7 @@ class Oystercard
     @balance = 0
   end
 
+  def top_up(amount)
+    @balance += amount
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,7 @@ describe Oystercard do
   it "checks balance" do
     expect(subject.balance).to eq 0
   end
+  it "tops up money in the existing balance" do
+    expect { subject.top_up(20) }.to change {subject.balance}.by 20
+  end
 end
