abef8bdd140914946c8ef82a12d41b7d03ea88d9
Sara <sarah.alalaya@gmail.com>
2016-07-13 08:02:36 +0300

    Refatcored both: touch_in & top_up methods

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,34 +1,50 @@
 class Oystercard
+  LIMIT = 90
+  MIN_BALANCE = 1
+  attr_accessor :balance
 
-LIMIT = 90
-MIN_BALANCE = 1
 
-attr_accessor :balance
+  def initialize
+    @balance = 0
+  end
 
-def initialize
-  @balance = 0
-end
+  def top_up(amount)
+    error = "Top up would exceed card limit of £#{LIMIT}"
+    raise error if balance_limit_exceeded?(amount)
+    @balance += amount
+  end
 
-def top_up(amount)
-  error = "Top up would exceed card limit of £#{LIMIT}"
-  raise error if (amount + @balance) > LIMIT
-  @balance += amount
-end
 
-def deduct(amount)
-  @balance -= amount
-end
+  def deduct(amount)
+    @balance -= amount
+  end
 
-def touch_in
-  raise 'Insufficient balance' if @balance < MIN_BALANCE
-  @in_journey = true
-end
+  def touch_in
+    error = 'Insufficient balance'
+    raise error if insufficient_balance?
+    @in_journey = true
+  end
 
-def touch_out
-  @in_journey = false
-end
+  def touch_out
+    @in_journey = false
+  end
 
-def in_journey?
-  @in_journey
-end
+  def in_journey?
+    @in_journey
+  end
+
+
+
+  private
+
+  def balance_limit_exceeded?(amount)
+    (amount + @balance) > LIMIT
+  end
+
+  def insufficient_balance?
+    @balance < MIN_BALANCE
+  end
+
+
+
 end
